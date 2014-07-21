1f168fd56b555cfa6b8e61df514cf3cfb673c5d9
Marc Hibbins <marchibbins@gmail.com>
2014-07-21 16:53:01 +0100

    Install Grunt with SASS setup

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    'css/main.min.css': '_sass/main.scss'
+                }
+            }
+        },
+        watch: {
+            css: {
+                files: '_sass/*.scss',
+                tasks: ['sass']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('compile', ['sass']);
+};
--- readme.md
+++ readme.md
@@ -12,4 +12,24 @@ A site built with [Jekyll](http://jekyllrb.com/).
 
 3. Browse to [http://localhost:4000](http://localhost:4000)
 
+
+### Development
+
+1. Install [SASS](http://sass-lang.com/)
+
+        $ gem install sass
+
+2. Install [Grunt](http://gruntjs.com/) and dependencies
+
+        $ npm install
+
+3. Compile and/or watch CSS
+
+        $ grunt compile
+        $ grunt watch
+
+4. Serve Jekyll with reload
+
+        $ jekyll serve --watch
+
 This work is licensed under a [Creative Commons Attribution-ShareAlike 3.0 Unported License](http://creativecommons.org/licenses/by-sa/3.0)
